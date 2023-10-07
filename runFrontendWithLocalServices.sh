afb9227996c61f3d069e21595741389d1ae517dc
Christian Folie <christian.folie@outlook.com>
2023-10-07 20:53:08 +0200

    updating .net version

--- runFrontendWithLocalServices.sh
+++ runFrontendWithLocalServices.sh
@@ -1,13 +1,12 @@
 #! /bin/bash
-
+# if you are using podman instead of docker,
+# use host.containers.internal instead of host.docker.internal to connect to the services on your host
 docker run --rm -it \
     -p 5000:5000 \
-    -v $(pwd)/customer:/customer \
     -e ASPNETCORE_URLS=http://+:5000 \
     -e DOTNET_ENVIRONMENT=DockerLocal \
     -e APIs__GuestExperience="http://host.docker.internal:8081/" \
     -e APIS__TableService="http://host.docker.internal:8082/" \
     -e APIS__Billing="http://host.docker.internal:8083/" \
-    mcr.microsoft.com/dotnet/sdk:6.0 \
-    dotnet run --no-launch-profile --project /customer/Customer.csproj
+    $(docker build -q customer)
  
